176cfd10aa876bd7988f9ed98eeb945cea81297c
jhunior-uscamayta <yukioshibuyabrt@gmail.com>
2024-10-29 08:36:02 -0500

    conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,10 +1,10 @@
 <?php
-require 'vendor/autoload.php'; // Solo si usas Composer para instalar MongoDB
+require 'vendor/autoload.php'; 
 
 try {
     $cliente = new MongoDB\Client("mongodb://localhost:27017");
-    $database = $cliente->nombre_base_datos; // Cambia "nombre_base_datos" por el nombre de tu base de datos
-    $collection = $database->nombre_coleccion; // Cambia "nombre_coleccion" por el nombre de tu colección
+    $database = $cliente->nombre_base_datos; 
+    $collection = $database->nombre_coleccion;
 } catch (Exception $e) {
     die("Error de conexión a MongoDB: " . $e->getMessage());
 }
